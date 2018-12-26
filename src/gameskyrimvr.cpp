835dfaa1d10f5e94105de31ab04c2301fc62fa76
LostDragonist <lost.dragonist@gmail.com>
2018-12-26 17:21:27 -0600

    Update plugin version

--- src/gameskyrimvr.cpp
+++ src/gameskyrimvr.cpp
@@ -117,7 +117,7 @@ QString GameSkyrimVR::description() const
 
 MOBase::VersionInfo GameSkyrimVR::version() const
 {
-  return VersionInfo(1, 3, 0, VersionInfo::RELEASE_FINAL);
+  return VersionInfo(1, 3, 1, VersionInfo::RELEASE_FINAL);
 }
 
 bool GameSkyrimVR::isActive() const
