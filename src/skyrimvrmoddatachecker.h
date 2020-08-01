ba071b6444d306333924c8f7be5a7689fb0b25a4
Mikaël Capelle <capelle.mikael@gmail.com>
2020-08-01 19:09:32 +0200

    Fix modgroups.

--- src/skyrimvrmoddatachecker.h
+++ src/skyrimvrmoddatachecker.h
@@ -20,7 +20,7 @@ protected:
   }
   virtual const FileNameSet& possibleFileExtensions() const override {
     static FileNameSet result{
-      "esp", "esm", "bsa", ".modgroups"
+      "esp", "esm", "bsa", "modgroups"
     };
     return result;
   }
